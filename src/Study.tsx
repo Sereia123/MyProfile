89543c9499fb80cd610761d2e2653466cf2b4e69
Taiki Masahisa <hiryuhazime11@gmail.com>
2025-05-24 20:17:37 +0900

    モンサクンチェック動画追加

--- src/Study.tsx
+++ src/Study.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 import {motion} from "framer-motion";
-import StudyImg from './StudyImg';
+import movie from './video/CheckExecute.mp4'
+import Monsakun from './img/Monsakun_ui.png';
 
 const Study = () => {
 
@@ -25,11 +26,33 @@ const Study = () => {
 
         <div>
           <p className='mt-4 indent-4'>
-            現実問題を数式で解決する数学的モデリングでは、数式を作ることのできる問題を作成することが重要視されており、先行研究となる「モンサクン」ではその作成要件を作問学習により学習者に学習させるシステムとなっている。本研究では、「モンサクン」において、学習者が自己説明により自身の作問演習が正しいかどうかを自身で判断することのできるシステム「モンサクン・チェック」の開発を行った。
+            現実問題を数式で解決する数学的モデリングでは、数式を作ることのできる問題を作成することが重要視されており、先行研究となる「モンサクン」ではその作成要件を作問学習により学習者に学習させるシステムとなっている。しかし、「モンサクン」における学習では、学習者自身が作成要件を見つけ出す発見的学習を行う場となっていることから、難しいと感じる学習者も存在する。本研究では、そのような学習者に対しての支援として、モンサクンを行う際にどのようなことを考えながら行えばいいかに対する道しるべを学習することのできるシステム「モンサクン・チェック」の設計・開発を行っている。
           </p>
         </div>
 
-        <StudyImg />
+        <div className='flex flex-col mt-5'>
+          <>
+            <img src={Monsakun} alt="モンサクンUI" className='w-[400px] max-sm:[300px]' />
+            <p className='mt-1 font-bold ml-[145px]'>図：モンサクン</p>
+          </>
+          
+          <>
+            <video className='mt-8 ml-auto w-[500px] max-sm:[300px] '  controls>
+              <source src={movie} type="video/mp4" />
+                お使いのブラウザは video タグに対応していません。
+            </video>
+            <p className='mt-1 font-bold ml-[300px]'>動画：モンサクン・チェック</p>
+
+            <p className='mt-2 ml-[220px]'>
+              モンサクン・チェックはこちら→
+              <a href="https://github.com/Sereia123/Monsakun_Check" target="_blank" rel="noopener noreferrer " className="text-blue-500 underline visited:text-purple-800">
+                モンサクン・チェック(開発中)
+              </a>
+            </p>
+          </>
+          
+        </div>
+        
       </motion.div>
     </section>
   )
